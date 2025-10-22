d579673939e6c83e17752753adfb0169812fa77e
Kotebus666 <kotebus666@gmail.com>
2025-10-22 10:55:11 +0300

    a11

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import "./index.css";
-//TODO: delete index.css
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
