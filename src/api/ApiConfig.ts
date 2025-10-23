ad3ddb7c8fd3ae88d874c5baeb12f3e73b72e11e
Kotebus666 <kotebus666@gmail.com>
2025-10-23 09:57:10 +0300

    Improve fetchQuestions

--- src/api/ApiConfig.ts
+++ src/api/ApiConfig.ts
@@ -1,9 +1,10 @@
 export const API_CONFIG = {
-    BASE_API_ENDPOINT_URL: "https://opentdb.com/api.php",
+    BASE_URL: "https://opentdb.com/",
+    API_ENDPOINT_URL: "api.php",
     QUESTIONS_REQUEST_KEY: "questions",
 
     REFRESH_INTERVAL: 0,
     ERROR_RETRY_INTERVAL: 1500,
     ERROR_RETRY_COUNT: 5,
-    SHOULD_RETRY_ON_ERROR: true,
+    SHOULD_RETRY_ON_ERROR: false,
 } as const;
--- src/api/trivia.ts
+++ src/api/trivia.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 import type { Question } from "../types/trivia";
 import {API_CONFIG} from "./ApiConfig.ts";
 
+interface ServerResponse {
+    results: Question[];
+}
+
 export const fetchQuestions = async (
     amount: number,
 ): Promise<Question[]> => {
@@ -10,7 +14,15 @@ export const fetchQuestions = async (
         return [];
     }
 
-    const url = new URL(API_CONFIG.BASE_API_ENDPOINT_URL);
-    url.searchParams.set("amount", String(amount));
-    return axios.get(url.toString()).then(r => r.data.results);
+    return axios.get<Question[]>(
+        API_CONFIG.API_ENDPOINT_URL,
+        {
+            baseURL: API_CONFIG.BASE_URL,
+            params: { amount },
+            transformResponse: (r) => {
+                const data = JSON.parse(r) as ServerResponse;
+                return data.results;
+            },
+        },
+    ).then(r => r.data);
 };
