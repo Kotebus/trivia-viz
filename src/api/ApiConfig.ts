8127052a4ebe23d529fe89fc06e21b34073028a5
Kotebus666 <kotebus666@gmail.com>
2025-10-23 09:58:02 +0300

    Ups, undo

--- src/api/ApiConfig.ts
+++ src/api/ApiConfig.ts
@@ -6,5 +6,5 @@ export const API_CONFIG = {
     REFRESH_INTERVAL: 0,
     ERROR_RETRY_INTERVAL: 1500,
     ERROR_RETRY_COUNT: 5,
-    SHOULD_RETRY_ON_ERROR: false,
+    SHOULD_RETRY_ON_ERROR: true,
 } as const;
